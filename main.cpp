f0ec5dd9dcd4884e91943de7cbd80939b564c79d
Stanislav Goriainov <goriainov@ispras.ru>
2023-03-02 14:19:25 +0300

    Add iterators support.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,38 @@
 #include <stdlike/vector.hpp>
+#include <vector>
+
+template <std::random_access_iterator T>
+void f(T) {
+}
+
+template <std::contiguous_iterator T>
+void g(T) {
+}
 
 int main() {
+    stdlike::Vector<int> test2(10, 9);
+    g(test2.begin());
+
+    test2.PushBack(10);
+    auto found2 = std::find(test2.begin(), test2.end(), 10);
+
+    if (found2 != test2.end()) {
+        std::cout << "Found" << std::endl;
+    } else {
+        std::cout << "Not found" << std::endl;
+    }
+
+    stdlike::Vector<bool> test(10, true);
+    f(test.begin());
+
+    test.PushBack(false);
+    auto found = std::find(test.begin(), test.end(), false);
+
+    if (found != test.end()) {
+        std::cout << "Found" << std::endl;
+    } else {
+        std::cout << "Not found" << std::endl;
+    }
+
     return 0;
 }
--- stdlike/vector.hpp
+++ stdlike/vector.hpp
@@ -1,9 +1,12 @@
 #ifndef STDLIKE_VECTOR_HPP
 #define STDLIKE_VECTOR_HPP
 
+#include <cstddef>
+#include <cstdint>
 #include <iostream>
 #include <cinttypes>
 #include <cassert>
+#include <iterator>
 
 #include <stdlike/move.hpp>
 #include <stdlike/forward.hpp>
@@ -12,6 +15,138 @@ namespace stdlike {
 
 template <typename Type>
 class Vector {
+public:
+    class Iterator : public std::iterator<std::contiguous_iterator_tag, Type> {
+    public:
+        Iterator() {
+        }
+
+        Iterator(Type* ptr) : ptr_(ptr) {
+        }
+
+        Iterator(const Iterator& other) : ptr_(other.ptr_) {
+        }
+
+        ~Iterator() {
+            ptr_ = nullptr;
+        }
+
+        Iterator& operator=(const Iterator& other) {
+            ptr_ = other.ptr_;
+        }
+
+        Type& operator*() const {
+            return *ptr_;
+        }
+
+        Type* operator->() const {
+            return ptr_;
+        }
+
+        Iterator& operator++() {
+            ++ptr_;
+            return *this;
+        }
+
+        Iterator operator++(int) {
+            Iterator temp = *this;
+            ++ptr_;
+            return temp;
+        }
+
+        Iterator& operator--() {
+            --ptr_;
+            return *this;
+        }
+
+        Iterator operator--(int) {
+            Iterator temp = *this;
+            --ptr_;
+            return temp;
+        }
+
+        Iterator& operator+=(ptrdiff_t diff) {
+            if (diff >= 0) {
+                while (diff--) {
+                    ++(*this);
+                }
+            } else {
+                while (diff++) {
+                    --(*this);
+                }
+            }
+        }
+
+        Iterator& operator-=(ptrdiff_t diff) {
+            return *this += -diff;
+        }
+
+        Type& operator[](ptrdiff_t diff) const {
+            return ptr_[diff];
+        }
+
+        friend Iterator operator+(const Iterator& iter, ptrdiff_t diff) {
+            Iterator temp = iter;
+            return temp += diff;
+        }
+
+        friend Iterator operator+(ptrdiff_t diff, const Iterator& iter) {
+            return iter + diff;
+        }
+
+        friend Iterator operator-(const Iterator& iter, ptrdiff_t diff) {
+            Iterator temp = iter;
+            return temp -= diff;
+        }
+
+        friend ptrdiff_t operator-(const Iterator& lhs, const Iterator& rhs) {
+            return lhs.ptr_ - rhs.ptr_;
+        }
+
+        friend bool operator==(const Iterator& lhs, const Iterator& rhs) {
+            return lhs.ptr_ == rhs.ptr_;
+        }
+
+        friend bool operator!=(const Iterator& lhs, const Iterator& rhs) {
+            return !(lhs == rhs);
+        }
+
+        friend bool operator<(const Iterator& lhs, const Iterator& rhs) {
+            return lhs.ptr_ < rhs.ptr_;
+        }
+
+        friend bool operator>(const Iterator& lhs, const Iterator& rhs) {
+            return rhs < lhs;
+        }
+
+        friend bool operator<=(const Iterator& lhs, const Iterator& rhs) {
+            return !(lhs > rhs);
+        }
+
+        friend bool operator>=(const Iterator& lhs, const Iterator& rhs) {
+            return !(lhs < rhs);
+        }
+
+    private:
+        Type* ptr_ = nullptr;
+    };
+
+    Iterator Begin() {
+        return Iterator(data_);
+    }
+
+    Iterator End() {
+        return Iterator(data_ + size_);
+    }
+
+    Iterator begin() {
+        return Begin();
+    }
+
+    Iterator end() {
+        return End();
+    }
+
 public:
     Vector() : size_(0), capacity_(0), char_data_(nullptr), data_(nullptr) {
     }
@@ -69,6 +204,8 @@ public:
 
         return *this;
     }
+
+    bool operator==(const Vector<Type>&) const = delete;
 
     /* Capacity */
 
@@ -216,10 +353,7 @@ private:
     }
 
     static inline size_t Release(Type* data, size_t start, size_t end) {
-        if (!data) {
-            return 0;
-        }
-
+        assert(data);
         for (size_t i = start; i < end; i++) {
             data[i].~Type();
         }
@@ -228,10 +362,7 @@ private:
     }
 
     static inline size_t Initialize(Type* data, size_t start, size_t end, const Type& value) {
-        if (!data) {
-            return 0;
-        }
-
+        assert(data);
         for (size_t cur_offset = start; cur_offset < end; cur_offset++) {
             new (data + cur_offset) Type(value);
         }
@@ -240,10 +371,7 @@ private:
     }
 
     static inline size_t Copy(Type* dest, size_t start, size_t end, const Type* src) {
-        if (!dest || !src) {
-            return 0;
-        }
-
+        assert(dest && src);
         for (size_t cur_offset = start; cur_offset < end; cur_offset++) {
             new (dest + cur_offset) Type(src[cur_offset]);
         }
@@ -261,59 +389,236 @@ private:
 template <>
 class Vector<bool> {
 public:
+    /* BitReference */
+
     class BitReference {
     public:
-        BitReference(uint8_t* source, size_t shift) : source_(source), shift_(shift) {
+        BitReference() : source_(nullptr), mask_(0) {
         }
 
-        BitReference(const BitReference& other) : source_(other.source_), shift_(other.shift_) {
+        BitReference(uint32_t* source, uint32_t mask) : source_(source), mask_(mask) {
+        }
+
+        BitReference(const BitReference& other) : source_(other.source_), mask_(other.mask_) {
         }
 
         ~BitReference() {
             source_ = nullptr;
-            shift_ = 0;
+            mask_ = 0;
         }
 
         BitReference& operator=(bool value) {
             if (value) {
-                uint8_t mask = static_cast<uint8_t>(1 << shift_);
-                *source_ |= mask;
+                *source_ |= mask_;
             } else {
-                uint8_t mask = static_cast<uint8_t>(~(1 << shift_));
-                *source_ &= mask;
+                *source_ &= ~mask_;
             }
-
             return *this;
         }
 
         BitReference& operator=(const BitReference& other) {
-            bool value = (1 << shift_) & *other.source_;
-            *this = value;
-
-            return *this;
+            return *this = bool(other);
         }
 
         operator bool() const {
-            return (1 << shift_) & *source_;
+            return mask_ & *source_;
+        }
+
+        friend bool operator==(const BitReference& lhs, const BitReference& rhs) {
+            return bool(lhs) == bool(rhs);
+        }
+
+        friend bool operator!=(const BitReference& lhs, const BitReference& rhs) {
+            return !(lhs == rhs);
+        }
+
+        friend bool operator<(const BitReference& lhs, const BitReference& rhs) {
+            return !bool(lhs) && bool(rhs);
+        }
+
+        friend bool operator>(const BitReference& lhs, const BitReference& rhs) {
+            return rhs < lhs;
+        }
+
+        friend bool operator<=(const BitReference& lhs, const BitReference& rhs) {
+            return !(lhs > rhs);
+        }
+
+        friend bool operator>=(const BitReference& lhs, const BitReference& rhs) {
+            return !(lhs < rhs);
         }
 
     private:
-        uint8_t* source_ = nullptr;
-        size_t shift_ = 0;
+        uint32_t* source_ = nullptr;
+        uint32_t mask_ = 0;
     };
 
 public:
+    /* Iterator */
+
+    class Iterator {
+    public:
+        /* Traits */
+
+        using iterator_category = std::random_access_iterator_tag;
+        using difference_type = int32_t;
+        using iterator = Iterator;
+
+        using value_type = bool;
+        using pointer = BitReference*;
+        using reference = BitReference;
+
+        Iterator() : data_(nullptr), shift_(0) {
+        }
+
+        Iterator(uint32_t* data, uint32_t shift) : data_(data), shift_(shift) {
+        }
+
+        Iterator(const Iterator& other) : data_(other.data_), shift_(other.shift_) {
+        }
+
+        ~Iterator() {
+            data_ = nullptr;
+            shift_ = 0;
+        }
+
+        Iterator& operator=(const Iterator& other) {
+            data_ = other.data_;
+            shift_ = other.shift_;
+            return *this;
+        }
+
+        reference operator*() const {
+            return reference(data_, 1u << shift_);
+        }
+
+        Iterator& operator++() {
+            if (shift_-- == 0) {
+                shift_ = 31;
+                data_++;
+            }
+            return *this;
+        }
+
+        Iterator operator++(int) {
+            Iterator old = *this;
+            ++(*this);
+            return old;
+        }
+
+        Iterator& operator--() {
+            if (shift_++ == 31) {
+                shift_ = 0;
+                data_--;
+            }
+            return *this;
+        }
+
+        Iterator operator--(int) {
+            Iterator old = *this;
+            --(*this);
+            return old;
+        }
+
+        Iterator& operator+=(difference_type diff) {
+            if (diff > 0) {
+                while (diff--) {
+                    ++(*this);
+                }
+            } else {
+                while (diff++) {
+                    --(*this);
+                }
+            }
+            return *this;
+        }
+
+        Iterator& operator-=(difference_type diff) {
+            return *this += -diff;
+        }
+
+        reference operator[](difference_type diff) const {
+            return *(*this + diff);
+        }
+
+        friend difference_type operator-(const Iterator& lhs, const Iterator& rhs) {
+            return static_cast<difference_type>(32 * (lhs.data_ - rhs.data_) + rhs.shift_ - lhs.shift_);
+        }
+
+        friend Iterator operator+(const Iterator& iter, int32_t diff) {
+            Iterator temp = iter;
+            return temp += diff;
+        }
+
+        friend Iterator operator+(int32_t diff, const Iterator& iter) {
+            return iter + diff;
+        }
+
+        friend Iterator operator-(const Iterator& iter, int32_t diff) {
+            Iterator temp = iter;
+            return temp -= diff;
+        }
+
+        friend bool operator==(const Iterator& lhs, const Iterator& rhs) {
+            return (lhs.data_ == rhs.data_ && lhs.shift_ == rhs.shift_);
+        }
+
+        friend bool operator!=(const Iterator& lhs, const Iterator& rhs) {
+            return !(lhs == rhs);
+        }
+
+        friend bool operator<(const Iterator& lhs, const Iterator& rhs) {
+            return (lhs.data_ < rhs.data_) || (lhs.data_ == rhs.data_ && lhs.shift_ > rhs.shift_);
+        }
+
+        friend bool operator>(const Iterator& lhs, const Iterator& rhs) {
+            return (rhs < lhs);
+        }
+
+        friend bool operator<=(const Iterator& lhs, const Iterator& rhs) {
+            return !(lhs > rhs);
+        }
+
+        friend bool operator>=(const Iterator& lhs, const Iterator& rhs) {
+            return !(lhs < rhs);
+        }
+
+
+    private:
+        uint32_t* data_ = nullptr;
+        uint32_t shift_ = 0;
+    };
+
+    Vector<bool>::Iterator Begin() {
+        return Iterator(data_, 31);
+    }
+
+    Vector<bool>::Iterator End() {
+        return Iterator(data_ + DivideByThirtyTwo(size_), 31 - ThirtyTwoModulo(size_));
+    }
+
+    Vector<bool>::Iterator begin() {
+        return Begin();
+    }
+
+    Vector<bool>::Iterator end() {
+        return End();
+    }
+
+public:
+    /* Vector<bool> */
+
     Vector() : size_(0), capacity_(0), data_(nullptr) {
     }
 
     explicit Vector(size_t init_size, bool value = false)
-        : size_(init_size), capacity_(RoundUpToEightMultiple(init_size)), data_(new uint8_t[BitsToBytes(capacity_)]()) {
+        : size_(init_size), capacity_(RoundUpToThirtyTwoMultiple(init_size)), data_(new uint32_t[BitsToBytes(capacity_)]()) {
 
         Initialize(data_, 0, size_, value);
     }
 
     Vector(const Vector<bool>& other)
-        : size_(other.Size()), capacity_(other.Capacity()), data_(new uint8_t[BitsToBytes(other.Capacity())]()) {
+        : size_(other.Size()), capacity_(other.Capacity()), data_(new uint32_t[BitsToBytes(other.Capacity())]()) {
 
         Copy(data_, 0, other.Size(), other.Data());
     }
@@ -332,7 +637,7 @@ public:
     Vector<bool>& operator=(const Vector<bool>& other) {
         size_ = other.Size();
         capacity_ = other.Capacity();
-        data_ = new uint8_t[BitsToBytes(other.Capacity())]();
+        data_ = new uint32_t[BitsToBytes(other.Capacity())]();
 
         Copy(data_, 0, other.Size(), other.Data());
 
@@ -346,6 +651,8 @@ public:
 
         return *this;
     }
+
+    bool operator==(const Vector<bool>&) const = delete;
 
     /* Capacity */
 
@@ -363,13 +670,13 @@ public:
 
     void Reserve(size_t new_capacity) {
         if (new_capacity > capacity_) {
-            this->ChangeCapacity(RoundUpToEightMultiple(new_capacity));
+            this->ChangeCapacity(RoundUpToThirtyTwoMultiple(new_capacity));
         }
     }
 
     void ShrinkToFit() {
         if (capacity_ > size_) {
-            this->ChangeCapacity(RoundUpToEightMultiple(size_));
+            this->ChangeCapacity(RoundUpToThirtyTwoMultiple(size_));
         }
     }
 
@@ -382,7 +689,7 @@ public:
 
     BitReference At(size_t pos) {
         assert(pos < size_);
-        return BitReference(data_ + (pos >> 3 /* (pos / 8) */), 7 - (pos & 7) /* (pos % 8) */);
+        return BitReference(data_ + DivideByThirtyTwo(pos), 1u << (31u - ThirtyTwoModulo(pos)));
     }
 
     const BitReference operator[](size_t pos) const {
@@ -391,7 +698,7 @@ public:
     }
 
     BitReference operator[](size_t pos) {
-        return BitReference(data_ + (pos >> 3 /* (pos / 8) */), 7 - (pos & 7) /* (pos % 8) */);
+        return BitReference(data_ + DivideByThirtyTwo(pos), 1u << (31u - ThirtyTwoModulo(pos)));
     }
 
     const BitReference Front() const {
@@ -412,12 +719,12 @@ public:
         return this->operator[](size_ - 1);
     }
 
-    const uint8_t* Data() const {
-        uint8_t* ret = const_cast<Vector<bool>*>(this)->Data();
-        return const_cast<const uint8_t*>(ret);
+    const uint32_t* Data() const {
+        uint32_t* ret = const_cast<Vector<bool>*>(this)->Data();
+        return const_cast<const uint32_t*>(ret);
     }
 
-    uint8_t* Data() {
+    uint32_t* Data() {
         return data_;
     }
 
@@ -438,7 +745,7 @@ public:
         }
         SetValue(data_, pos, value);
 
-        return BitReference(data_ + (pos >> 3) /* (pos / 8) */, 7 - (pos & 7) /* (pos % 8) */);
+        return BitReference(data_ + DivideByThirtyTwo(pos), 1u << (31u - ThirtyTwoModulo(pos)));
     }
 
     void Erase(size_t pos) {
@@ -462,7 +769,7 @@ public:
 
     void Resize(size_t new_size, bool value = false) {
         if (size_ < new_size) {
-            this->Reserve(RoundUpToEightMultiple(new_size));
+            this->Reserve(RoundUpToThirtyTwoMultiple(new_size));
             Initialize(data_, size_, new_size, value);
             size_ = new_size;
         }
@@ -478,8 +785,8 @@ private:
     /* Helper functions */
 
     void ChangeCapacity(size_t new_capacity) {
-        new_capacity = RoundUpToEightMultiple(new_capacity);
-        uint8_t* new_data = new uint8_t[BitsToBytes(new_capacity)];
+        new_capacity = RoundUpToThirtyTwoMultiple(new_capacity);
+        uint32_t* new_data = new uint32_t[BitsToBytes(new_capacity)];
         size_t new_size = Copy(new_data, 0, std::min(size_, new_capacity), data_);
         this->~Vector();
 
@@ -488,11 +795,8 @@ private:
         data_ = new_data;
     }
 
-    static inline size_t Initialize(uint8_t* data, size_t start, size_t end, bool value) {
-        if (!data) {
-            return 0;
-        }
-
+    static inline size_t Initialize(uint32_t* data, size_t start, size_t end, bool value) {
+        assert(data);
         for (size_t cur_pos = start; cur_pos < end; cur_pos++) {
             SetValue(data, cur_pos, value);
         }
@@ -500,11 +804,8 @@ private:
         return end - start;
     }
 
-    static inline size_t Copy(uint8_t* dest, size_t start, size_t end, const uint8_t* src) {
-        if (!dest || !src) {
-            return 0;
-        }
-
+    static inline size_t Copy(uint32_t* dest, size_t start, size_t end, const uint32_t* src) {
+        assert(dest && src);
         for (size_t cur_pos = start; cur_pos < end; cur_pos++) {
             SetValue(dest, cur_pos, GetValue(src, cur_pos));
         }
@@ -512,29 +813,38 @@ private:
         return end - start;
     }
 
-    static inline bool GetValue(const uint8_t* data, size_t pos) {
-        size_t shift = 7 - /* (pos % 8) */ (pos & 7);
-        return *(data + /* (pos / 8) */ (pos >> 3)) & (1 << shift);
+    static inline bool GetValue(const uint32_t* data, size_t pos) {
+        BitReference bit_ref(const_cast<uint32_t*>(data) + DivideByThirtyTwo(pos), 1u << (31u - ThirtyTwoModulo(pos)));
+        return bool(bit_ref);
     }
 
-    static inline void SetValue(uint8_t* data, size_t pos, bool value) {
-        size_t shift = 7 - /* (pos % 8) */ (pos & 7);
-        BitReference bit_ref(data + /* (pos / 8) */ (pos >> 3), shift);
+    static inline void SetValue(uint32_t* data, size_t pos, bool value) {
+        BitReference bit_ref(data + DivideByThirtyTwo(pos), 1u << (31u - ThirtyTwoModulo(pos)));
         bit_ref = value;
     }
+
+    /* Utility functions */
 
     static inline uint64_t BitsToBytes(uint64_t bits) {
         return bits >> 3;
     }
 
-    static inline uint64_t RoundUpToEightMultiple(uint64_t num) {
-        return (num + 7) & ~7ull;
+    static inline uint32_t RoundUpToThirtyTwoMultiple(uint64_t num) {
+        return (num + 31) & ~31u;
+    }
+
+    static inline uint32_t DivideByThirtyTwo(uint64_t num) {
+        return static_cast<uint32_t>(num >> 5);
+    }
+
+    static inline uint32_t ThirtyTwoModulo(uint64_t num) {
+        return num & 31;
     }
 
 private:
     size_t size_ = 0;     /* in bits */
     size_t capacity_ = 0; /* in bits, always == 0 (mod 8), at least 8 */
-    uint8_t* data_ = nullptr;
+    uint32_t* data_ = nullptr;
 };
 
 template <class Type>
